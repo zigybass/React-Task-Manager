ed4777cf1926e561efc9f95d1ea18b3a09bc3cee
Zigy <zigybass@gmail.com>
2019-10-20 14:38:49 -0400

    need to update state in tasklist

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,27 +3,36 @@ import { Collapsible, CollapsibleItem } from "react-materialize";
 
 class TaskList extends React.Component {
 
-  onDelete = (id) => {
-    console.log("hello " + id)
+  state = {
+    tasks: []
   }
 
-    render () {
+  onDelete = id => {
+    console.log("hello " + id);
 
-      return (
+  };
+
+  render() {
+    return (
       <div>
         <h4 style={{ textAlign: "center" }}>Task List</h4>
         <Collapsible accordion={false}>
-          {this.props.tasks.map(task => {
+          {this.props.tasks.map((task, i) => {
             return (
-              <CollapsibleItem id={task.key} key={task.key} header={task.newTask}>
+              <CollapsibleItem
+                id={task.key}
+                key={task.key}
+                header={task.newTask} 
+                icon={<i onClick={this.onDelete.bind(this, i)} className="material-icons" id={task.key}>delete_outline</i>}
+              >
                 {task.details}
               </CollapsibleItem>
             );
           })}
-          </Collapsible>
+        </Collapsible>
       </div>
     );
-   }
+  }
 }
 
 export default TaskList;
