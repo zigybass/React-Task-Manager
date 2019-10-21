4b3efbf67be4bc304869741dd64e67e28c67b625
Zigy <zigybass@gmail.com>
2019-10-21 14:34:33 -0400

    DidUpdate now sets state. working on deleting

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Collapsible, CollapsibleItem } from "react-materialize";
+import { Collapsible, CollapsibleItem } from "react-materialize"
 
 class TaskList extends React.Component {
 
@@ -7,23 +7,36 @@ class TaskList extends React.Component {
     tasks: []
   }
 
+  componentDidUpdate () {
+    if (this.props.tasks !== this.state.tasks) {
+      this.setState({ tasks: this.props.tasks});
+    }
+  }
+
   onDelete = id => {
     console.log("hello " + id);
-
+    console.log(this.state.tasks)
+    this.setState({ tasks: 
+      this.state.tasks.filter( () => {
+        return (id !== this.state.tasks.key)
+      })  
+    })
   };
 
   render() {
+
     return (
       <div>
         <h4 style={{ textAlign: "center" }}>Task List</h4>
         <Collapsible accordion={false}>
           {this.props.tasks.map((task, i) => {
+            const { key } = task;
             return (
               <CollapsibleItem
-                id={task.key}
+                id={key}
                 key={task.key}
                 header={task.newTask} 
-                icon={<i onClick={this.onDelete.bind(this, i)} className="material-icons" id={task.key}>delete_outline</i>}
+                icon={<i onClick={this.onDelete.bind(this, key)} className="material-icons" id={key}>delete_outline</i>}
               >
                 {task.details}
               </CollapsibleItem>
