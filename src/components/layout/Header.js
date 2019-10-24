7d1ce19c34c75e6a47de7b3c237e59cb1def0006
Zigy <zigybass@gmail.com>
2019-10-24 17:11:11 -0400

    fixed footer so content shows at botton

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,10 +7,10 @@ function Header() {
     <div>
       <nav>
         <div className="nav-wrapper blue-grey darken-1">
-          <div className="brand-logo topHeader">
+          <div className="brand-logo title">
             <i className="material-icons">assignment</i>Task Manager
           </div>
-          <ul className="right hide-on-med-and-down topHeader">
+          <ul className="right hide-on-med-and-down title">
             <li>
               {/* <Link to="/login" className="waves-effect lime accent-2 black-text waves-dark btn">
                 Login
